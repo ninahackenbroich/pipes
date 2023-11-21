96f9711d7814a0c070d7ba1eacd9a783ddcd8ece
Nina Hackenbroich <ninahackenbroich@MBP-von-Nina.fritz.box>
2023-11-21 12:30:52 +0100

    built-in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'pipes-payment';
+  name: string = '';
+  date: string = '';
+  value: number = 0;
+  miles: number = 0;
+
+  onNameChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const newName = inputElement.value;
+    this.name = newName;
+  }
+
+  onDateChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const newDate = inputElement.value;
+    this.date = newDate;
+  }
+
+  onValueChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const newValue = inputElement.value;
+    this.value = parseFloat(newValue);
+  }
+
+  onMilesChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const newMiles = inputElement.value;
+    this.miles = parseFloat(newMiles);
+  }
+
 }
